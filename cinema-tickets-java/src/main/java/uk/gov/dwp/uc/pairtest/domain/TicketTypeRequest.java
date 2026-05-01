28f54fe3f4898061ee3303494f422d2357559e02
Christopher Corday <corday@gmail.com>
2026-05-01 11:58:15 +0100

    Make TicketTypeRequest a record to ensure immutability

--- cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/domain/TicketTypeRequest.java
+++ cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/domain/TicketTypeRequest.java
@@ -4,23 +4,7 @@ package uk.gov.dwp.uc.pairtest.domain;
  * Immutable Object
  */
 
-public class TicketTypeRequest {
-
-    private int noOfTickets;
-    private Type type;
-
-    public TicketTypeRequest(Type type, int noOfTickets) {
-        this.type = type;
-        this.noOfTickets = noOfTickets;
-    }
-
-    public int getNoOfTickets() {
-        return noOfTickets;
-    }
-
-    public Type getTicketType() {
-        return type;
-    }
+public record TicketTypeRequest(Type type, int noOfTickets) {
 
     public enum Type {
         ADULT, CHILD , INFANT
